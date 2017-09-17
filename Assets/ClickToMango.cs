ceba5ec3ac14e1258c32699eba1b0dd9288398a7
sophiaaar <sophiaclarke93@gmail.com>
2017-09-17 13:39:58 +0100

    Click To Mango scripts
    The editor window script and the accompanying ‘Click To Mango’ script

---
+++ Assets/ClickToMango.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class ClickToMango : MonoBehaviour {
+
+	public GameObject mango;
+
+	public static bool mangoEnabled
+	{
+		get
+		{
+		#if UNITY_EDITOR
+            return EditorPrefs.GetBool("mangoEnabled", false);
+		#else
+            return false;
+		#endif
+        }
+
+        set
+        {
+		#if UNITY_EDITOR
+            EditorPrefs.SetBool("mangoEnabled", value);
+		#endif
+        }
+	}
+
+	void Update()
+	{
+		if (mangoEnabled == true)
+		{
+			//Debug.Log("true");
+			mango.SetActive(true);
+		}
+		else
+		{
+			mango.SetActive(false);
+		}
+	}
+}
---
+++ Assets/Editor/Window.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class Window : EditorWindow {
+
+	[MenuItem ("Window/Sophia's Window")]
+    public static void ShowWindow()
+    {
+        GetWindow<Window>(false, "Click To Mango", true);
+    }
+
+	void OnGUI()
+	{
+		GUILayout.Label ("Click To Mango", EditorStyles.boldLabel);
+
+        ClickToMango.mangoEnabled = EditorGUILayout.Toggle("Mango Toggle", ClickToMango.mangoEnabled);
+	}
+}
